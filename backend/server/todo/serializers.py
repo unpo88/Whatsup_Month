0ec0820f4df2f5ddc5298dff03b7912404a247c9
Whatsup <whatsup@lemonbase.com>
2020-08-01 16:56:06 +0900

    TODO CRUD API 작업

--- backend/server/todo/serializers.py
+++ backend/server/todo/serializers.py
@@ -1,8 +1,32 @@
 from rest_framework import serializers
 from .models import Todo
+from django.db import transaction
 
 
 class TodoSerializer(serializers.ModelSerializer):
     class Meta:
         model = Todo
-        fields = '__all__'
+        exclude = ('id', 'created_at')
+        read_only_fields = ('is_completed', )
+
+    def _get_extracted_data(self, data):
+        copied_data = data.copy()
+        extracted_data = {
+            'title': copied_data.pop('title', None),
+            'description': copied_data.pop('description', None),
+        }
+        return extracted_data
+
+    @transaction.atomic
+    def create(self, validated_data):
+        extracted_data = self._get_extracted_data(validated_data)
+
+        todo = Todo.objects.create(
+            title=extracted_data['title'],
+            description=extracted_data['description'],
+        )
+
+        return todo
+
+    def validate(self, attrs):
+        return attrs
--- backend/server/todo/views.py
+++ backend/server/todo/views.py
@@ -1,6 +1,6 @@
-from django.db import transaction
 from rest_framework import viewsets
-from rest_framework.permissions import IsAuthenticated
+from django.db import transaction
+from rest_framework.decorators import action
 from rest_framework.response import Response
 
 from .models import Todo
@@ -8,5 +8,25 @@ from .serializers import TodoSerializer
 
 
 class TodoViewSet(viewsets.ModelViewSet):
+    lookup_field = 'entity_id'
+    lookup_url_kwarg = 'todo_entity_id'
     queryset = Todo.objects.all()
     serializer_class = TodoSerializer
+    http_method_names = ['get', 'post', 'put', 'delete', 'options']
+
+    @transaction.atomic
+    def delete(self, request, *args, **kwargs):
+        return super().destroy(request, args, kwargs)
+
+    @action(methods=['PUT'],
+            url_path='status',
+            detail=True,
+            url_name='update-status')
+    @transaction.atomic
+    def update_status(self, request, todo_entity_id=None):
+        todo = Todo.objects.get(entity_id=todo_entity_id)
+
+        todo.is_completed = request.data.get('is_completed')
+        todo.save()
+
+        return Response(None, 200)
