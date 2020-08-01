e749b7bd6568ec685523efeed3b7079967f0593d
Whatsup <whatsup@lemonbase.com>
2020-08-01 17:25:14 +0900

    Todo Frontend 작업

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import { Button, Form, Input, Checkbox } from 'antd';
+import { Button, Form, Input, Table, Space } from 'antd';
 
 import '../../static/frontend/main.css';
 
@@ -21,40 +21,90 @@ export class App extends Component {
     const onFinishFailed = (errorInfo) => {
       console.log('Failed:', errorInfo);
     };
+
+    const columns = [
+      {
+        title: 'Title',
+        dataIndex: 'title',
+        key: 'title',
+        render: (text) => <a>{text}</a>,
+      },
+      {
+        title: 'Description',
+        dataIndex: 'description',
+        key: 'description',
+      },
+      {
+        title: 'is_completed',
+        dataIndex: 'is_completed',
+        key: 'address',
+      },
+      {
+        title: 'Action',
+        key: 'action',
+        render: (text, record) => (
+          <Space size="middle">
+            <a>Delete</a>
+          </Space>
+        ),
+      },
+    ];
+
+    const data = [
+      {
+        key: '1',
+        title: 'John Brown',
+        description: 32,
+      },
+      {
+        key: '2',
+        title: 'Jim Green',
+        description: 42,
+      },
+      {
+        key: '3',
+        title: 'Joe Black',
+        description: 32,
+      },
+    ];
+
     return (
-      <Form
-        {...layout}
-        name="basic"
-        initialValues={{ remember: true }}
-        onFinish={onFinish}
-        onFinishFailed={onFinishFailed}
-      >
-        <Form.Item
-          label="Username"
-          name="username"
-          rules={[{ required: true, message: 'Please input your username!' }]}
-        >
-          <Input />
-        </Form.Item>
+      <div className="container">
+        <header>
+          <h1>Todo Layout</h1>
+        </header>
+        <section className="content">
+          <section className="todo-form">
+            <Form
+              {...layout}
+              name="basic"
+              initialValues={{ remember: true }}
+              onFinish={onFinish}
+              onFinishFailed={onFinishFailed}
+            >
+              <Form.Item label="title" name="title" rules={[{ required: true, message: 'Please input todo title!' }]}>
+                <Input placeholder="제목을 입력하세요." />
+              </Form.Item>
 
-        <Form.Item
-          label="Password"
-          name="password"
-          rules={[{ required: true, message: 'Please input your password!' }]}
-        >
-          <Input.Password />
-        </Form.Item>
+              <Form.Item
+                label="description"
+                name="description"
+                rules={[{ required: true, message: 'Please input todo description!' }]}
+              >
+                <Input placeholder="설명을 입력하세요." />
+              </Form.Item>
 
-        <Form.Item {...tailLayout} name="remember" valuePropName="checked">
-          <Checkbox>Remember me</Checkbox>
-        </Form.Item>
-
-        <Form.Item {...tailLayout}>
-          <Button type="primary" htmlType="submit">
-            Submit
-          </Button>
-        </Form.Item>
-      </Form>
+              <Form.Item {...tailLayout}>
+                <Button type="primary" htmlType="submit">
+                  Submit
+                </Button>
+              </Form.Item>
+            </Form>
+            <Table columns={columns} dataSource={data} />,
+          </section>
+        </section>
+        <footer>하단</footer>
+      </div>
     );
   }
 }
